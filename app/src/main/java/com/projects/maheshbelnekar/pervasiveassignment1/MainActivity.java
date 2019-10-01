686cf6b549312ed2da82f9e86337a05fcb4ece0d
Mahesh Belnekar <mbelnek@iu.edu>
2019-10-01 17:05:43 -0400

    initial version of the app: created first 2 activities

---
+++ app/src/main/java/com/projects/maheshbelnekar/pervasiveassignment1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.projects.maheshbelnekar.pervasiveassignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText phoneEditText;
+    private EditText emailEditText;
+    private Button sumbitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize all variables
+        nameEditText = findViewById(R.id.name);
+        phoneEditText = findViewById(R.id.phone);
+        emailEditText = findViewById(R.id.email);
+        sumbitButton = findViewById(R.id.submit);
+
+        // attach listener
+        sumbitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private Boolean loginUser() {
+        // Extract values first
+        String name,phone,email;
+        name = nameEditText.getText().toString();
+        phone = phoneEditText.getText().toString();
+        email = emailEditText.getText().toString();
+
+        Intent loginIntent = new Intent(MainActivity.this, Profile.class);
+        loginIntent.putExtra("name",name);
+        loginIntent.putExtra("phone",phone);
+        loginIntent.putExtra("email",email);
+
+        Toast.makeText(this,name+phone+email,Toast.LENGTH_SHORT).show();
+
+        startActivity(loginIntent);
+
+        return true;
+    }
+}
